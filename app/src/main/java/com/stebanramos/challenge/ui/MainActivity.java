6e47ba0ae39c1c39a4a6e2c5dd8cfa1f9521e731
Steban Ramos <juanramosiam@gmail.com>
2022-04-19 21:23:57 -0500

    Input material desing

--- app/src/main/java/com/stebanramos/challenge/ui/MainActivity.java
+++ app/src/main/java/com/stebanramos/challenge/ui/MainActivity.java
@@ -2,13 +2,19 @@ package com.stebanramos.challenge.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.stebanramos.challenge.BuildConfig;
 import com.stebanramos.challenge.databinding.ActivityMainBinding;
@@ -20,7 +26,6 @@ public class MainActivity extends AppCompatActivity {
 
 
     private EditText etSearch;
-    private Button btnSearch;
     private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,16 +39,20 @@ public class MainActivity extends AppCompatActivity {
 
     private void configView(){
         etSearch = binding.etSearch;
-        btnSearch = binding.btnSearch;
+        etSearch.requestFocus();
+        etSearch.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+                    Preferences.Set_str(MainActivity.this, "search", etSearch.getText().toString());
+                    Log.d(TAG, "configView() search " + etSearch.getText());
 
-        btnSearch.setOnClickListener(view -> {
-
-            Intent intent = new Intent(MainActivity.this, SearchActivity.class);
-            Preferences.Set_str(this, "search", etSearch.getText().toString());
-            Log.d(TAG, "configView() search " + etSearch.getText());
-
-            startActivity(intent);
-
+                    startActivity(intent);
+                    return true;
+                }
+                return false;
+            }
         });
     }
 }
