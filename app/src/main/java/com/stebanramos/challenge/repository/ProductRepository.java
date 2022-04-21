7cf0e4d84fb21f31d0d2fd51d30177bac408d908
Steban Ramos <juanramosiam@gmail.com>
2022-04-20 23:30:20 -0500

    bug request searchView

--- app/src/main/java/com/stebanramos/challenge/repository/ProductRepository.java
+++
@@ -1,6 +0,0 @@
-package com.stebanramos.challenge.repository;
-
-public interface ProductRepository {
-
-
-}
--- app/src/main/java/com/stebanramos/challenge/viewModels/SearchViewModel.java
+++ app/src/main/java/com/stebanramos/challenge/viewModels/SearchViewModel.java
@@ -39,6 +39,7 @@ public class SearchViewModel extends ViewModel {
     private JSONObject installments;
     private boolean free_shipping;
 
+    private RequestQueue mQueue;
     private List<Product> productList = new ArrayList<>();
     public MutableLiveData<String> searchInput = new MutableLiveData<>();
     private MutableLiveData<List<Product>> muProductList;
@@ -65,55 +66,61 @@ public class SearchViewModel extends ViewModel {
         Log.d(TAG, "loadData()");
 
         try {
-            VolleySingleton.getInstance(context).addToRequestQueue(
+            mQueue = VolleySingleton.getInstance(context).getRequestQueue();
+            Uri baseUri = Uri.parse(SEARCH_PRODCUTS_URL);
+            Uri.Builder builder = baseUri.buildUpon();
 
-                    new GsonRequest<JSONObject>(SEARCH_ITEM_URL + "ids"+ Preferences.Get_str(context, "itemId"),
-                            null,
-                            null,
-                            new Response.Listener<JSONObject>() {
-                                @Override
-                                public void onResponse(JSONObject response) {
-                                    try {
-                                        //get the array called results
-                                        JSONArray results = response.getJSONArray("results");
-                                        Log.d(TAG, "loadData() onResponse " + results);
+            builder.appendQueryParameter("q", searchInput.getValue());
 
-                                        //iterate in every object inside the array
-                                        for (int i = 0; i < results.length(); i++) {
-                                            JSONObject products = results.getJSONObject(i);
+            Log.d(TAG, "loadData() uri " + builder);
 
-                                            Log.d(TAG, "loadData() onResponse " + results.getJSONObject(i));
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, builder.toString(), null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            try {
+                                //progressBar.setVisibility(View.GONE);
+                                //get the array called results
+                                JSONArray results = response.getJSONArray("results");
+                                Log.d(TAG, "loadData() onResponse " + results);
 
-                                            id = products.getString("id");
-                                            title = products.getString("title");
-                                            price = products.getString("price");
-                                            available_quantity = products.getString("available_quantity");
-                                            condition = products.getString("condition");
-                                            permalink = products.getString("permalink");
-                                            thumbnail = products.getString("thumbnail");
-                                            attributes = products.getString("attributes");
-                                            category_id = products.getString("category_id");
-                                            installments = products.getJSONObject("installments");
-                                            free_shipping = products.getJSONObject("shipping").getBoolean("free_shipping");
+                                //iterate in every object inside the array
+                                for (int i = 0; i < results.length(); i++) {
+                                    JSONObject products = results.getJSONObject(i);
 
-                                            productList.add(new Product(id, title, price, available_quantity, condition, permalink, thumbnail, attributes, category_id, installments, free_shipping));
-                                        }
-                                        muProductList.setValue(productList);
+                                    Log.d(TAG, "loadData() onResponse " + results.getJSONObject(i));
 
-                                    } catch (JSONException e) {
-                                        e.printStackTrace();
-                                    }
+
+                                    id = products.getString("id");
+                                    title = products.getString("title");
+                                    price = products.getString("price");
+                                    available_quantity = products.getString("available_quantity");
+                                    condition = products.getString("condition");
+                                    permalink = products.getString("permalink");
+                                    thumbnail = products.getString("thumbnail");
+                                    attributes = products.getString("attributes");
+                                    category_id = products.getString("category_id");
+                                    installments = products.getJSONObject("installments");
+                                    free_shipping = products.getJSONObject("shipping").getBoolean("free_shipping");
+
+                                    productList.add(new Product(id, title, price, available_quantity, condition, permalink, thumbnail, attributes, category_id, installments, free_shipping));
                                 }
-                            },
-                            new Response.ErrorListener() {
-                                @Override
-                                public void onErrorResponse(VolleyError error) {
-                                    Log.d(TAG, "Error Volley:" + error.getMessage());
-                                    error.printStackTrace();
-                                }
+                                muProductList.setValue(productList);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
                             }
-                    )
-            );
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            //just print the error and notify the user for some technical problems
+                            error.printStackTrace();
+                            Toast.makeText(context, "Something went wrong", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+            mQueue.add(request);
         } catch (Exception e) {
             Utils.printtCatch(e, "loadData", TAG);
         }
